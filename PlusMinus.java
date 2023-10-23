a47ee6090a5644fecd7122f118872ae67d9e3b79
Digiphoenix22 <75356380+Digiphoenix22@users.noreply.github.com>
2023-10-23 12:32:43 -0400

    CQ6 Submission!
    IcecreamParlor was a tough one... I think we're good though.

---
+++ PlusMinus.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    public static void plusMinus(List<Integer> arr) {
+    int positive = 0, negative = 0, zero = 0;
+    int size = arr.size();
+
+    for (int num : arr) {
+        if (num > 0) {
+            positive++;
+        } else if (num < 0) {
+            negative++;
+        } else {
+            zero++;
+        }
+    }
+
+    System.out.printf("%.6f\n", (double) positive / size);
+    System.out.printf("%.6f\n", (double) negative / size);
+    System.out.printf("%.6f\n", (double) zero / size);
+    }
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.plusMinus(arr);
+
+        bufferedReader.close();
+    }
+}
